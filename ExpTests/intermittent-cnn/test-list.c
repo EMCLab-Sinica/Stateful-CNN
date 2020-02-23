a08fc9ba7a69afcde399635574cc617b5e3bb78c
Chih-Hsuan Yen <yan12125@gmail.com>
2020-02-23 10:10:16 +0800

    Try to build FreeRTOS for Linux
    portmacro.h is from https://github.com/sean93park/freertos-linux/blob/master/portable/portmacro.h

---
+++ ExpTests/intermittent-cnn/test-list.c
@@ -0,0 +1,29 @@
+#include <FreeRTOS.h>
+#include <list.h>
+#include <stdio.h>
+
+static const size_t LIST_SIZE = 5;
+
+int main() {
+    List_t list;
+    ListItem_t items[LIST_SIZE];
+    int values[LIST_SIZE];
+
+    vListInitialise(&list);
+    for (size_t i = 0; i < LIST_SIZE; i++) {
+        vListInitialiseItem(items + i);
+        values[i] = i;
+        listSET_LIST_ITEM_OWNER(items + i, values + i);
+        vListInsertEnd(&list, items + i);
+    }
+
+    int *valptr = NULL;
+    for (size_t i = 0; i < LIST_SIZE * 2; i++) {
+        listGET_OWNER_OF_NEXT_ENTRY(valptr, &list);
+        printf("%d ", *valptr);
+    }
+
+    printf("\n");
+
+    return 0;
+}
