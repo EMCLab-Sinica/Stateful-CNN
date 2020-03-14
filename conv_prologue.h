39599f826ae9198daaa9775f7cf8039372bc40db
Chih-Hsuan Yen <yan12125@gmail.com>
2020-03-14 20:36:31 +0800

    Merge conv*.h back to conv.c

--- conv_prologue.h
+++
@@ -1,4 +0,0 @@
-/* put var declarations first to make the compiler happy */
-int16_t *input_addr, *filter_addr;
-/* Cannot use C as a variable name here as C is a macro on MSP430 :( */
-uint16_t H, W, kH, kW, CHANNEL;
