4b756ac54a61f6d7a81b7e8e1ce0579eb6544849
Chih-Hsuan Yen <yan12125@gmail.com>
2020-03-14 18:35:10 +0800

    First step of removing pipelining

--- ExpTests/intermittent-cnn/conv_pre.h
+++ ExpTests/intermittent-cnn/conv_pre.h
@@ -1,74 +1,59 @@
 {
-#ifdef USE_CONCURRENT_CONV
-    /* TODO lazy init */
-    arrH[uxIndex] = conv_params->conv_input->dims[1];
-    arrW[uxIndex] = conv_params->conv_input->dims[2];
-    arrkH[uxIndex] = conv_params->conv_filter->dims[1];
-    arrkW[uxIndex] = conv_params->conv_filter->dims[2];
-    arrCHANNEL[uxIndex] = conv_params->conv_filter->dims[3];
-
-    H = arrH[uxIndex];
-    W = arrW[uxIndex];
-    kH = arrkH[uxIndex];
-    kW = arrkW[uxIndex];
-    CHANNEL = arrCHANNEL[uxIndex];
-#endif
-
     /* copy filter data */
-    if (!filter_buffer_addr[conv_params->conv_idx]) {
-        kH = conv_params->conv_filter->dims[1];
-        kW = conv_params->conv_filter->dims[2];
-        CHANNEL = conv_params->conv_filter->dims[3];
+    if (!filter_buffer_addr[conv_params.conv_idx]) {
+        kH = conv_params.conv_filter->dims[1];
+        kW = conv_params.conv_filter->dims[2];
+        CHANNEL = conv_params.conv_filter->dims[3];
         filter_addr = get_q15_param(
-            conv_params->conv_filter,
-            (size_t)(conv_params->conv_idx * CHANNEL * kH * kW));
-        uint16_t buffer_size = sizeof(int16_t) * mac_params[uxIndex].length;
+            conv_params.conv_filter,
+            (size_t)(conv_params.conv_idx * CHANNEL * kH * kW));
+        uint16_t buffer_size = sizeof(int16_t) * mac_params.length;
         int16_t filter_offset = kH * global_conv_params.dest_offset;
-        filter_buffer_addr[conv_params->conv_idx] = (int16_t*)buffer_iq31_mac_results(NUM_TASKS - 1) - filter_offset * (filter_buffer_id + 1);
+        filter_buffer_addr[conv_params.conv_idx] = (int16_t*)iq31_mac_results - filter_offset * (filter_buffer_id + 1);
 
         if (global_conv_params.truncated) {
-            int16_t *current_filter_buffer_addr = filter_buffer_addr[conv_params->conv_idx];
+            int16_t *current_filter_buffer_addr = filter_buffer_addr[conv_params.conv_idx];
             for (uint16_t h = 0; h < kH; h++) {
                 my_memcpy(current_filter_buffer_addr, filter_addr, kW * CHANNEL * sizeof(int16_t));
                 current_filter_buffer_addr += global_conv_params.dest_offset;
                 filter_addr += kW * CHANNEL;
             }
         } else {
-            my_memcpy(filter_buffer_addr[conv_params->conv_idx],
+            my_memcpy(filter_buffer_addr[conv_params.conv_idx],
                       filter_addr,
                       buffer_size);
             if (global_conv_params.truncated) {
                 // dummy value
-                filter_buffer_addr[conv_params->conv_idx][buffer_size / sizeof(int16_t) - 1] = 0;
+                filter_buffer_addr[conv_params.conv_idx][buffer_size / sizeof(int16_t) - 1] = 0;
             }
         }
         if (cached_filter_idx[filter_buffer_id] >= 0) {
             filter_buffer_addr[cached_filter_idx[filter_buffer_id]] = NULL;
         }
-        cached_filter_idx[filter_buffer_id] = conv_params->conv_idx;
+        cached_filter_idx[filter_buffer_id] = conv_params.conv_idx;
         filter_buffer_id++;
         if (filter_buffer_id == global_conv_params.filter_limit) {
             filter_buffer_id = 0;
         }
     }
 
-    uint8_t scheduled_filters = (conv_params->flags & 0x00ff) >> 1;
+    uint8_t scheduled_filters = (conv_params.flags & 0x00ff) >> 1;
     my_printf_debug("scheduled_filters = %d" NEWLINE, scheduled_filters);
-    my_printf_debug("conv_params->output_h = %d" NEWLINE, conv_params->output_h);
-    my_printf_debug("conv_params->starting_output_h = %d" NEWLINE, conv_params->starting_output_h);
-    if (scheduled_filters == 0 || (scheduled_filters == 1 && conv_params->output_h < conv_params->starting_output_h)) {
-        kH = conv_params->conv_filter->dims[1];
-        W = conv_params->conv_input->dims[2];
-        CHANNEL = conv_params->conv_filter->dims[3];
+    my_printf_debug("conv_params.output_h = %d" NEWLINE, conv_params.output_h);
+    my_printf_debug("conv_params.starting_output_h = %d" NEWLINE, conv_params.starting_output_h);
+    if (scheduled_filters == 0 || (scheduled_filters == 1 && conv_params.output_h < conv_params.starting_output_h)) {
+        kH = conv_params.conv_filter->dims[1];
+        W = conv_params.conv_input->dims[2];
+        CHANNEL = conv_params.conv_filter->dims[3];
         int8_t field_size = (int8_t)((kH - 1) / 2);
 
         /* copy input data, row by row */
 
         input_addr = get_q15_param(
-            conv_params->conv_input,
+            conv_params.conv_input,
             (size_t)(CHANNEL * (
-                    (conv_params->output_h) * W +
-                    (conv_params->output_w)
+                    (conv_params.output_h) * W +
+                    (conv_params.output_w)
             )));
 
         /* int32_t instead of int16_t as TI's compiler cannot handle negative
@@ -81,23 +66,23 @@
          * In step 2, R11 becomes 0x01FFFC, while it should be -4, or 0x00FFFC,
          * and thus the resultant address is offset by 0x10000.
          */
-        int32_t w_start = int16_max(-field_size,    -conv_params->output_w),
-                w_end   = int16_min( field_size, W-1-conv_params->output_w);
+        int32_t w_start = int16_max(-field_size,    -conv_params.output_w),
+                w_end   = int16_min( field_size, W-1-conv_params.output_w);
         int16_t *src = NULL,
                 *dest;
         int16_t src_offset = W * CHANNEL;
         uint16_t inputs_len = LEA_BUFFER_SIZE - 4 - global_conv_params.filter_limit * kH * global_conv_params.dest_offset;
-        if (conv_params->do_reinitialize_input) {
-            next_input_buffer_addr = lea_buffer + conv_params->starting_output_h_offset * global_conv_params.dest_offset;
+        if (conv_params.do_reinitialize_input) {
+            next_input_buffer_addr = lea_buffer + conv_params.starting_output_h_offset * global_conv_params.dest_offset;
         }
 
-        dest = input_buffer_addr[uxIndex] = next_input_buffer_addr;
+        dest = input_buffer_addr = next_input_buffer_addr;
 
-        H = conv_params->conv_input->dims[1];
+        H = conv_params.conv_input->dims[1];
         int32_t h_start,
-                h_end = int16_min(field_size, H-1-conv_params->output_h);
+                h_end = int16_min(field_size, H-1-conv_params.output_h);
 
-        if (conv_params->do_reinitialize_input) {
+        if (conv_params.do_reinitialize_input) {
             my_printf_debug("Reinitialize input buffer" NEWLINE);
 
             if (scheduled_filters == 0) {
@@ -109,7 +94,7 @@
                 msp_checkStatus(status);
             }
 
-            h_start = int16_max(-field_size, -conv_params->output_h);
+            h_start = int16_max(-field_size, -conv_params.output_h);
         } else {
             h_start = field_size;
         }
@@ -131,10 +116,10 @@
             }
         }
     } else{
-        if (conv_params->do_reinitialize_input) {
+        if (conv_params.do_reinitialize_input) {
             next_input_buffer_addr = lea_buffer;
         }
-        input_buffer_addr[uxIndex] = next_input_buffer_addr;
+        input_buffer_addr = next_input_buffer_addr;
     }
 
     /* XXX: assume stride=1 */
