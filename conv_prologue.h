f39b5aeb8c9985b3f0285da42dee29a9377ac9df
Chih-Hsuan Yen <yan12125@gmail.com>
2020-03-14 17:54:12 +0800

    More optimizations
    * Enable optimization flags
    * Move more stuffs out of task loop
    * Get rid of some warnings

--- conv_prologue.h
+++ conv_prologue.h
@@ -1,6 +1,5 @@
 /* put var declarations first to make the compiler happy */
 ConvTaskParams *conv_params = &arr_conv_params[uxIndex];
 int16_t *input_addr, *filter_addr;
-uint16_t buffer_size;
 /* Cannot use C as a variable name here as C is a macro on MSP430 :( */
-uint16_t H, W, kH, kW, CHANNEL, OUTPUT_CHANNEL;
+uint16_t H, W, kH, kW, CHANNEL;
