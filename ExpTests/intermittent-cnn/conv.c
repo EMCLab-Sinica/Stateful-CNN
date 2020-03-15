c99aa722d2e75f66cbb097464d77da5e4d5a6b07
Chih-Hsuan Yen <yan12125@gmail.com>
2020-03-15 23:09:24 +0800

    Use matrix multiplication instead of MAC
    Done on PC, not yet correct on MSP430 as columns and rows should be even
    for msp_matrix_mpy_q15 [1]
    
    [1] http://e2e.ti.com/support/microcontrollers/msp430/f/166/t/716353?MSP430FR5992-MSP-DSPLib-msp-matrix-mpy-q15

--- ExpTests/intermittent-cnn/conv.c
+++ ExpTests/intermittent-cnn/conv.c
@@ -15,9 +15,9 @@
 
 // TODO: make these adjustable on runtime
 #define TILE_W 1
+#define OUTPUT_LEN 10
 
 int16_t *input_buffer_addr;
-int16_t *next_input_buffer_addr;
 
 #define CONV_TASK_FLAG_PROCESSED_FILTERS_BASE 2
 typedef struct ConvTaskParams {
@@ -30,7 +30,6 @@ typedef struct ConvTaskParams {
     uint16_t output_h;
     uint16_t output_w;
     uint16_t flags;
-    uint8_t do_reinitialize_input;
     uint8_t tile_h;
     OpExtraData *extra_data;
 } ConvTaskParams;
@@ -44,32 +43,31 @@ static struct {
     uint16_t W_by_OUTPUT_CHANNEL;
 } global_conv_params;
 
-static msp_mac_q15_params mac_params;
+static msp_matrix_mpy_q15_params matrix_mpy_params;
 static int16_t *filter_buffer_addr[NUM_FILTERS];  // filter index -> address
 static int8_t cached_filter_idx[NUM_FILTERS];  // filter buffer id (0~filter_limit-1) -> filter index
 static int8_t filter_buffer_id;
 static uint8_t pending_filters[NUM_FILTERS];
 static uint8_t pending_filter_idx = 0;
 
-int32_t *iq31_mac_results = (int32_t*)(lea_buffer + LEA_BUFFER_SIZE) - 1;
+int16_t *matrix_mpy_results = lea_buffer + LEA_BUFFER_SIZE - OUTPUT_LEN;
 
-static void convTask(void) {
+static void convTask(uint8_t offset_h, uint8_t tile_h) {
     /* put var declarations first to make the compiler happy */
     int16_t *filter_addr;
     /* Cannot use C as a variable name here as C is a macro on MSP430 :( */
     uint16_t kH, kW, CHANNEL;
+    kH = conv_params.conv_filter->dims[1];
 
     /* copy filter data */
     if (!filter_buffer_addr[conv_params.conv_idx]) {
-        kH = conv_params.conv_filter->dims[1];
         kW = conv_params.conv_filter->dims[2];
         CHANNEL = conv_params.conv_filter->dims[3];
         filter_addr = get_q15_param(
             conv_params.conv_filter,
             (size_t)(conv_params.conv_idx * CHANNEL * kH * kW));
-        uint16_t buffer_size = sizeof(int16_t) * mac_params.length;
         int16_t filter_offset = kH * global_conv_params.dest_offset;
-        filter_buffer_addr[conv_params.conv_idx] = (int16_t*)iq31_mac_results - filter_offset * (filter_buffer_id + 1);
+        filter_buffer_addr[conv_params.conv_idx] = matrix_mpy_results - filter_offset * (filter_buffer_id + 1);
 
         if (global_conv_params.truncated) {
             int16_t *current_filter_buffer_addr = filter_buffer_addr[conv_params.conv_idx];
@@ -79,6 +77,7 @@ static void convTask(void) {
                 filter_addr += kW * CHANNEL;
             }
         } else {
+            uint16_t buffer_size = sizeof(int16_t) * global_conv_params.dest_offset * kH;
             my_memcpy(filter_buffer_addr[conv_params.conv_idx],
                       filter_addr,
                       buffer_size);
@@ -97,73 +96,72 @@ static void convTask(void) {
         }
     }
 
-    my_printf_debug("conv_params.output_h = %d" NEWLINE, conv_params.output_h);
-    if (conv_params.do_reinitialize_input) {
-        next_input_buffer_addr = lea_buffer;
-    }
-    input_buffer_addr = next_input_buffer_addr;
+    my_printf_debug("conv_params.output_h = %d" NEWLINE, conv_params.output_h + offset_h);
+    input_buffer_addr = lea_buffer + offset_h * global_conv_params.dest_offset;
 
     /* XXX: assume stride=1 */
-    next_input_buffer_addr += global_conv_params.dest_offset; // dest_offset already calibrated for truncation
-    my_printf_debug("Increment next_input_buffer_addr" NEWLINE);
-    my_printf_debug("next_input_buffer_addr = lea_buffer + %d" NEWLINE, (int)(next_input_buffer_addr - lea_buffer));
-    if (next_input_buffer_addr > lea_buffer + LEA_BUFFER_SIZE) {
-        ERROR_OCCURRED();
-    }
 
-    msp_status status = msp_mac_q15(&mac_params,
-                                    input_buffer_addr,
-                                    filter_buffer_addr[conv_params.conv_idx],
-                                    iq31_mac_results);
+    matrix_mpy_params.srcARows = (tile_h - offset_h + kH - 1) / kH;
+    matrix_mpy_params.srcACols = matrix_mpy_params.srcBRows = kH * global_conv_params.dest_offset;
+    matrix_mpy_params.srcBCols = 1;
+
+    msp_status status = msp_matrix_mpy_q15(
+        &matrix_mpy_params,
+        input_buffer_addr,
+        filter_buffer_addr[conv_params.conv_idx],
+        matrix_mpy_results
+    );
     msp_checkStatus(status);
 
     /* START dump data */
     my_printf_debug("conv_idx=%d ", conv_params.conv_idx);
-    my_printf_debug("output_h=%d ", conv_params.output_h);
+    my_printf_debug("output_h=%d ", conv_params.output_h + offset_h);
     my_printf_debug("output_w=%d" NEWLINE, conv_params.output_w);
 
     my_printf_debug("input_buffer_addr = lea_buffer + %d" NEWLINE, (int)(input_buffer_addr - lea_buffer));
     my_printf_debug("input" NEWLINE);
-    dump_matrix(input_buffer_addr, mac_params.length);
+    dump_matrix2(input_buffer_addr, matrix_mpy_params.srcARows, matrix_mpy_params.srcACols);
     my_printf_debug("filter_buffer_addr = lea_buffer + LEA_BUFFER_SIZE - %d" NEWLINE, (int)(lea_buffer + LEA_BUFFER_SIZE - filter_buffer_addr[conv_params.conv_idx]));
     my_printf_debug("filter" NEWLINE);
-    dump_matrix(filter_buffer_addr[conv_params.conv_idx], mac_params.length);
+    // print transposed matrix for easier debugging
+    dump_matrix2(filter_buffer_addr[conv_params.conv_idx], matrix_mpy_params.srcBCols, matrix_mpy_params.srcBRows);
 
-    my_printf_debug("iq31_mac_result=");
-    print_iq31_debug(*iq31_mac_results);
+    my_printf_debug("matrix_mpy_results=");
+    dump_matrix(matrix_mpy_results, matrix_mpy_params.srcARows);
     my_printf_debug(NEWLINE);
     /* END dump data */
 
-    int16_t q15_mac_result = iq31_to_q15(*iq31_mac_results);
-    q15_mac_result += *get_q15_param(conv_params.bias, conv_params.conv_idx);
-
-    my_printf_debug("after adding bias OFM value=");
-    print_q15_debug(q15_mac_result);
-    my_printf_debug(NEWLINE);
-
-    if ((conv_params.flags >> 8) & CONV_ACTIVATIONS_RELU) {
-        q15_mac_result = MAX_VAL(q15_mac_result, 0);
-    }
-
+    // TODO: use LEA?
     int16_t *output_data = get_q15_param(conv_params.output, 0);
-    size_t offset = (size_t)(conv_params.output_h * global_conv_params.W_by_OUTPUT_CHANNEL + conv_params.output_w * global_conv_params.OUTPUT_CHANNEL + conv_params.conv_idx);
-    my_printf_debug("offset of output_data=%" PRIsize_t NEWLINE, offset);
-    output_data[offset] = q15_mac_result;
+    int16_t bias_value = *get_q15_param(conv_params.bias, conv_params.conv_idx);
+    for (uint8_t idx = 0; idx < matrix_mpy_params.srcARows; idx++) {
+        int16_t q15_mac_result = matrix_mpy_results[idx];
+        q15_mac_result += bias_value;
+
+        my_printf_debug("after adding bias OFM value=");
+        print_q15_debug(q15_mac_result);
+        my_printf_debug(NEWLINE);
+
+        if (conv_params.flags & CONV_ACTIVATIONS_RELU) {
+            q15_mac_result = MAX_VAL(q15_mac_result, 0);
+        }
+        size_t offset = (conv_params.output_h + offset_h + idx * kH) * global_conv_params.W_by_OUTPUT_CHANNEL + conv_params.output_w * global_conv_params.OUTPUT_CHANNEL + conv_params.conv_idx;
+        my_printf_debug("offset of output_data=%" PRIsize_t NEWLINE, offset);
+        output_data[offset] = q15_mac_result;
+    }
 }
 
-static inline void schedule_tile(uint16_t idx, uint16_t output_h, uint16_t output_w, uint8_t tile_h, uint8_t tile_w, uint16_t H, uint16_t W) {
+static inline void schedule_tile(uint16_t idx, uint16_t output_h, uint16_t output_w, uint8_t tile_h, uint8_t tile_w, uint16_t W) {
     OpExtraData *extra_data = conv_params.extra_data;
     extra_data->current_filter = idx;
     conv_params.conv_idx = idx;
     conv_params.tile_h = tile_h;
+    uint16_t kH = conv_params.conv_filter->dims[1];
     for (uint8_t i = 0; i < MIN_VAL(tile_w, W - output_w); i++) {
-        for (uint8_t j = 0; j < MIN_VAL(tile_h, H - output_h); j++) {
-            conv_params.flags &= 0xff00;
-            conv_params.output_h = output_h + j;
+        for (uint8_t j = 0; j < kH; j++) {
+            conv_params.output_h = output_h;
             conv_params.output_w = output_w + i;
-            my_printf_debug("j = %d" NEWLINE, j);
-            conv_params.do_reinitialize_input = (j == 0);
-            convTask();
+            convTask(j, tile_h);
         }
     }
     extra_data->processed_filters[idx] = 1;
@@ -198,9 +196,9 @@ static inline void handle_conv_inner_loop(uint16_t n_conv, uint16_t output_h, ui
     int16_t *src = NULL,
             *dest;
     int16_t src_offset = W * CHANNEL;
-    uint16_t inputs_len = LEA_BUFFER_SIZE - 4 - global_conv_params.filter_limit * kH * global_conv_params.dest_offset;
+    uint16_t inputs_len = LEA_BUFFER_SIZE - OUTPUT_LEN - global_conv_params.filter_limit * kH * global_conv_params.dest_offset;
 
-    dest = input_buffer_addr = next_input_buffer_addr = lea_buffer;
+    dest = lea_buffer;
 
     H = conv_params.conv_input->dims[1];
     int32_t h_start = int16_max(      -field_size,    -output_h),
@@ -231,7 +229,7 @@ static inline void handle_conv_inner_loop(uint16_t n_conv, uint16_t output_h, ui
     }
 
     if (extra_data->current_filter) {
-        schedule_tile(extra_data->current_filter, output_h, output_w, tile_h, tile_w, H, W);
+        schedule_tile(extra_data->current_filter, output_h, output_w, tile_h, tile_w, W);
     }
     for (uint8_t idx = 0; idx < n_conv; idx++) {
         if (extra_data->processed_filters[idx]) {
@@ -239,7 +237,7 @@ static inline void handle_conv_inner_loop(uint16_t n_conv, uint16_t output_h, ui
             continue;
         }
         if (filter_buffer_addr[idx]) {
-            schedule_tile(idx, output_h, output_w, tile_h, tile_w, H, W);
+            schedule_tile(idx, output_h, output_w, tile_h, tile_w, W);
         } else {
             my_printf_debug("Filter %d is not cached, append it to the pending list" NEWLINE, idx);
             pending_filters[pending_filter_idx] = idx;
@@ -248,7 +246,7 @@ static inline void handle_conv_inner_loop(uint16_t n_conv, uint16_t output_h, ui
     }
     for (uint8_t idx = 0; idx < pending_filter_idx; idx++) {
         uint8_t filter_idx = pending_filters[idx];
-        schedule_tile(filter_idx, output_h, output_w, tile_h, tile_w, H, W);
+        schedule_tile(filter_idx, output_h, output_w, tile_h, tile_w, W);
         my_printf_debug("Mark filter %d as processed" NEWLINE, filter_idx);
     }
     pending_filter_idx = 0;
@@ -286,9 +284,8 @@ uint8_t handle_conv(ParameterInfo *input[], ParameterInfo *output, OpExtraData *
     conv_params.bias = bias;
     conv_params.output = output;
     conv_params.extra_data = extra_data;
-    conv_params.flags = flags << 8;
+    conv_params.flags = flags;
     input_buffer_addr = NULL;
-    next_input_buffer_addr = NULL;
 
     if (!extra_data->conv_running) {
         extra_data->conv_idx = extra_data->output_h = extra_data->output_w = 0;
@@ -320,20 +317,18 @@ uint8_t handle_conv(ParameterInfo *input[], ParameterInfo *output, OpExtraData *
     global_conv_params.W_by_OUTPUT_CHANNEL = W * global_conv_params.OUTPUT_CHANNEL;
 
     /* MSP430 LEA requires length to be even */
-    mac_params.length = (uint16_t)(CHANNEL * kH * kW / 2 * 2);
-    global_conv_params.truncated = (mac_params.length != CHANNEL * kH * kW);
+    global_conv_params.truncated = (CHANNEL * kH * kW / 2 * 2 != CHANNEL * kH * kW);
     if (global_conv_params.truncated) {
         // when CHANNEL * kH * kW is odd, CHANNEL * kW (dest_offset) is
         // also odd, so dummy values are needed between slices to make
         // addresses even.
         // a dummy value for each slice (kW * CHANNEL q15 values)
-        mac_params.length += kH + 1;
         global_conv_params.dest_offset++;
     }
 
     global_conv_params.filter_limit = MIN_VAL(
         conv_filter->dims[0],
-        (LEA_BUFFER_SIZE - 4 - global_conv_params.dest_offset * (kH + tile_h - 1)) / (global_conv_params.dest_offset * kH)
+        (LEA_BUFFER_SIZE - OUTPUT_LEN - global_conv_params.dest_offset * (kH + tile_h - 1)) / (global_conv_params.dest_offset * kH)
     );
 
     my_printf_debug("filter_limit: %d" NEWLINE, global_conv_params.filter_limit);
