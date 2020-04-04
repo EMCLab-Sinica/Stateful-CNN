c63929768d5bab1067502cd3ad843af81e52a568
Chih-Hsuan Yen <yan12125@gmail.com>
2020-04-04 12:10:59 +0800

    Vendor only needed files from TI DSPLib

---
+++ TI-DSPLib/vendor.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -ex
+TI_DSPLIB_PATH="$1"
+
+[ -n "$TI_DSPLIB_PATH" ] || exit 1
+
+rm -rv include source
+mkdir -p source/{lea,matrix,vector,utility}
+cp -rv "$TI_DSPLIB_PATH"/include .
+cp -rv "$TI_DSPLIB_PATH"/source/lea ./source/
+cp -v "$TI_DSPLIB_PATH"/source/matrix/msp_matrix_mpy_q15.c ./source/matrix
+cp -v "$TI_DSPLIB_PATH"/source/vector/msp_add_q15.c ./source/vector
+cp -v "$TI_DSPLIB_PATH"/source/utility/{msp_interleave_q15.c,msp_fill_q15.c} ./source/utility
