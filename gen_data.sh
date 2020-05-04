0c2b7fa059e097604bc7a5294df7a19a81b844c7
Chih-Hsuan Yen <yan12125@gmail.com>
2020-05-04 16:07:45 +0800

    Commit data

--- gen_data.sh
+++ gen_data.sh
@@ -1,8 +1,9 @@
-DATA_PATH=..
-MODEL=$DATA_PATH/models/mnist/model_optimized.onnx
-INPUT_FILE=$DATA_PATH/MNIST/Test-28x28_cntk_text.txt
-#INPUT_FILE=$DATA_PATH/example3.png
+# https://github.com/onnx/models/raw/master/vision/classification/mnist/model/mnist-8.onnx
+MODEL=data/mnist-8.onnx
+INPUT_FILE=data/Test-28x28_cntk_text.txt
+#INPUT_FILE=data/example3.png
 
+python optimize_model.py $MODEL
 python gen_ops.py
 # TODO: add --with-progress-embedding back after fixing the performance regression
-python transform.py $MODEL $INPUT_FILE
+python transform.py ${MODEL/.onnx/_optimized.onnx} $INPUT_FILE
