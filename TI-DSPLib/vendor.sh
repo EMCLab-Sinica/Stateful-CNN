8ce40cc5c1fb014adf2904dfb731ecb614fbf7b7
Chih-Hsuan Yen <yan12125@gmail.com>
2020-06-16 20:23:12 +0800

    TI-DSPLib: add msp_scale_q15 for scaling back OFM values in conv

--- TI-DSPLib/vendor.sh
+++ TI-DSPLib/vendor.sh
@@ -9,6 +9,6 @@ mkdir -p source/{lea,matrix,vector,utility}
 cp -rv "$TI_DSPLIB_PATH"/include .
 cp -rv "$TI_DSPLIB_PATH"/source/lea ./source/
 cp -v "$TI_DSPLIB_PATH"/source/matrix/msp_matrix_mpy_q15.c ./source/matrix
-cp -v "$TI_DSPLIB_PATH"/source/vector/msp_add_q15.c ./source/vector
+cp -v "$TI_DSPLIB_PATH"/source/vector/{msp_add_q15.c,msp_scale_q15.c} ./source/vector
 cp -v "$TI_DSPLIB_PATH"/source/utility/{msp_interleave_q15.c,msp_fill_q15.c} ./source/utility
 patch -Np1 -F0 -i ./eliminate-warnings.diff
