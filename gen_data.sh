c69b807fedb286d5bbc8bd0cd0349f051c317f46
Chih-Hsuan Yen <yan12125@gmail.com>
2020-06-19 13:35:03 +0800

    gen_data.sh: style fix
    Make it easier to find what lines to comment/uncomment when switching
    models/datasets.

--- gen_data.sh
+++ gen_data.sh
@@ -1,5 +1,6 @@
 # https://github.com/onnx/models/raw/master/vision/classification/mnist/model/mnist-8.onnx
 # NEED_OPTIMIZE: onnx optimizer cannot handle squeezenet_cifar10.onnx, so skip it
+
 #MODEL=data/mnist-8.onnx
 #INPUT_FILE=data/Test-28x28_cntk_text.txt
 #NEED_OPTIMIZE=1
