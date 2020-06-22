753b390cc3282de63e1c49a55ffe21beb72af1be
Chih-Hsuan Yen <yan12125@gmail.com>
2020-06-22 17:26:20 +0800

    Fix command for running MNIST tests

--- run-models.sh
+++ run-models.sh
@@ -1,2 +1,2 @@
-python onnx_run.py data/cifar10-test_batch data/squeezenet_cifar10.onnx
+python onnx_run.py data/Test-28x28_cntk_text.txt data/mnist-8_optimized.onnx
 python keras_run.py data/cifar10-test_batch data/SqueezeNet_vs_CIFAR10
