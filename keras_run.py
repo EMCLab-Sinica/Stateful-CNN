2a20721d85ab3b0966eea7b6996598d4b4e71eb5
Chih-Hsuan Yen <yan12125@gmail.com>
2020-06-26 18:25:07 +0800

    keras_run: don't print feature maps of each layer when predicting multiple images
    Also go back to upstream SqueezeNet-CIFAR10 repo

--- keras_run.py
+++ keras_run.py
@@ -1,17 +1,15 @@
 import os.path
 import sys
 
+import numpy as np
 import tensorflow as tf
 from tensorflow.keras import backend as K
 
 from utils import load_data_cifar10
 
-# argv[2] should be path to https://github.com/zshancock/SqueezeNet_vs_CIFAR10
-sys.path.append(sys.argv[2])
-
-from squeezenet_architecture import SqueezeNet
-
-model = SqueezeNet()
+with open(os.path.join(sys.argv[2], 'models', 'squeeze_net.json')) as f:
+    model_json = f.read()
+model = tf.keras.models.model_from_json(model_json)
 model.load_weights(os.path.join(sys.argv[2], 'models', 'squeeze_net.h5'))
 
 # Modified from https://stackoverflow.com/a/41712013/3786245
@@ -19,24 +17,36 @@ inp = model.input                                           # input placeholder
 outputs = [layer.output for layer in model.layers]          # all layer outputs
 functors = [K.function([inp], [out]) for out in outputs]    # evaluation functions
 
+limit = 1
 # Testing
-labels, images = load_data_cifar10(sys.argv[1], limit=1)
-layer_outs = [func([images]) for func in functors]
-for idx in range(len(layer_outs)):
-    layer_out = layer_outs[idx][0]
-    shape = tf.shape(layer_out)
-    print(model.layers[idx].__class__.__name__.split('.')[-1], end='')
-    print(f' layer: {model.layers[idx].name}')
-    print(f'Shape: {shape}')
-    if tf.shape(shape)[0] == 4:
-        N, H, W, C = shape
-        assert N == 1
-        for c in range(C):
-            print(f'Channel {c}')
-            for h in range(H):
-                for w in range(W):
-                    print('%13.6f' % layer_out[0, h, w, c], end='')
+labels, images = load_data_cifar10(sys.argv[1], limit=limit)
+if limit == 1:
+    layer_outs = [func([images]) for func in functors]
+    for idx in range(len(layer_outs)):
+        layer_out = layer_outs[idx][0]
+        shape = tf.shape(layer_out)
+        print(model.layers[idx].__class__.__name__.split('.')[-1], end='')
+        print(f' layer: {model.layers[idx].name}')
+        print(f'Shape: {shape}')
+        if tf.shape(shape)[0] == 4:
+            N, H, W, C = shape
+            assert N == 1
+            for c in range(C):
+                print(f'Channel {c}')
+                for h in range(H):
+                    for w in range(W):
+                        print('%13.6f' % layer_out[0, h, w, c], end='')
+                    print()
                 print()
-            print()
-    else:
-        print(layer_out)
+        else:
+            print(layer_out)
+else:
+    correct = 0
+    for idx, image in enumerate(images):
+        layer_outs = model(image)
+        # Tensorflow 2.x uses .numpy instead of .eval for eager execution
+        predicted = np.argmax(layer_outs.numpy()[0])
+        if predicted == labels[idx]:
+            correct += 1
+    total = len(labels)
+    print(f'correct={correct} total={total} rate={correct/total}')
