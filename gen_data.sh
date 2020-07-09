bf3e1822f4fd1d792ce7dcab5ca65efc561edd5b
Chih-Hsuan Yen <yan12125@gmail.com>
2020-07-09 18:53:20 +0800

    Go back to MNIST

--- gen_data.sh
+++ gen_data.sh
@@ -1,12 +1,12 @@
 # https://github.com/onnx/models/raw/master/vision/classification/mnist/model/mnist-8.onnx
 # NEED_OPTIMIZE: onnx optimizer cannot handle squeezenet_cifar10.onnx, so skip it
 
-#MODEL=data/mnist-8.onnx
-#INPUT_FILE=data/Test-28x28_cntk_text.txt
-#NEED_OPTIMIZE=1
-MODEL=data/squeezenet_cifar10.onnx
-INPUT_FILE=data/cifar10-test_batch
-NEED_OPTIMIZE=0
+MODEL=data/mnist-8.onnx
+INPUT_FILE=data/Test-28x28_cntk_text.txt
+NEED_OPTIMIZE=1
+#MODEL=data/squeezenet_cifar10.onnx
+#INPUT_FILE=data/cifar10-test_batch
+#NEED_OPTIMIZE=0
 
 if [[ "$NEED_OPTIMIZE" = 1 ]] ; then
     python optimize_model.py $MODEL
