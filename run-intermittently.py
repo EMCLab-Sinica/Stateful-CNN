8a753f160ad784f1b6d1beab8dd9db0d2ce983cd
Chih-Hsuan Yen <yan12125@gmail.com>
2020-07-16 15:28:37 +0800

    run-intermittently: detect crashes & use specified binary path

--- run-intermittently.py
+++ run-intermittently.py
@@ -1,13 +1,17 @@
 from subprocess import Popen, TimeoutExpired
 import signal
+import sys
 
 def main():
     while True:
-        with Popen(['./out/intermittent-cnn', '1']) as proc:
+        with Popen([sys.argv[1], '1']) as proc:
             try:
                 proc.wait(timeout=0.001)
             except TimeoutExpired:
                 proc.send_signal(signal.SIGINT)
+            proc.wait()
+            if proc.returncode in (-signal.SIGABRT, -signal.SIGFPE):
+                raise Exception('Crashed')
             if proc.returncode == 0:
                 break
 
