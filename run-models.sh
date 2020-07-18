701cf602d8724edceb1fef0a8a2fce5b56593179
Chih-Hsuan Yen <yan12125@gmail.com>
2020-07-18 18:15:40 +0800

    There are no more on-disk optimized models

--- run-models.sh
+++ run-models.sh
@@ -1,2 +1,2 @@
-python onnx_run.py data/Test-28x28_cntk_text.txt data/mnist-8_optimized.onnx
+python onnx_run.py data/Test-28x28_cntk_text.txt data/mnist-8.onnx
 python keras_run.py data/cifar10-test_batch data/SqueezeNet_vs_CIFAR10
