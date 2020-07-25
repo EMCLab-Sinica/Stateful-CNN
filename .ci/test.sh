07272eeb4eb8f8553ce89246110588f4d1d19a0c
Chih-Hsuan Yen <yan12125@gmail.com>
2020-07-25 21:35:58 +0800

    ci: actually use --all-samples in all configurations

--- .ci/test.sh
+++ .ci/test.sh
@@ -4,9 +4,9 @@ pacman -S --noconfirm --needed base-devel cmake python-pip
 pip install --user numpy onnx
 
 # preparation
-args='--all-samples'
+args="--all-samples"
 if [[ $WITH_PROGRESS_EMBEDDING = 0 ]]; then
-    args='--without-progress-embedding'
+    args="$args --without-progress-embedding"
 fi
 
 python transform.py $args "$CONFIG"
