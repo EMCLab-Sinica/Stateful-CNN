489779371433dec448aa191c62de13f25fde1554
Chih-Hsuan Yen <yan12125@gmail.com>
2020-07-30 14:03:30 +0800

    Fix debug build

--- .ci/test.sh
+++ .ci/test.sh
@@ -1,19 +1,24 @@
 # dependencies
+
+set -e
+
 pacman -Syu --noconfirm
 pacman -S --noconfirm --needed base-devel cmake python-pip
 pip install --user numpy onnx
 
 # preparation
-args="--all-samples"
+transform_args="--all-samples"
+run_args=""
 if [[ $WITH_PROGRESS_EMBEDDING = 0 ]]; then
-    args="$args --without-progress-embedding"
+    transform_args="$transform_args --without-progress-embedding"
 fi
 
 if [[ $DEBUG_BUILD = 1 ]]; then
     sed -i 's/#define MY_NDEBUG//' debug.h
+    run_args="$run_args 1"
 fi
 
-python transform.py $args "$CONFIG"
+python transform.py $transform_args "$CONFIG"
 cmake -B build
 make -C build
-./build/intermittent-cnn
+./build/intermittent-cnn $run_args
