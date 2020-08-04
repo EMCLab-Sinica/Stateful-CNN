b76743b7dbecf359c9c998da6038681e1e3f1ec8
Chih-Hsuan Yen <yan12125@gmail.com>
2020-08-05 00:00:46 +0800

    Fix building on Cent OS 7

--- .ci/test-centos7.sh
+++ .ci/test-centos7.sh
@@ -1,8 +1,10 @@
 # dependencies
 yum -y update
-yum -y install gcc-c++ make cmake python36
+yum -y install gcc-c++ make cmake python36 wget
 python3.6 -m ensurepip --user
 pip3 install --user dataclasses numpy onnx
+
+./data/download-mnist.sh
 
 python3.6 transform.py mnist
 mkdir build
