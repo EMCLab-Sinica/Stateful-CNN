0be2a7ff81d89d62c889cb6572107f8e551a4d27
Chih-Hsuan Yen <yan12125@gmail.com>
2020-08-14 14:34:36 +0800

    ARM-CMSIS: Link needed files only
    * To get rid of huge exclusion list in CCStudio
    * Remove the need of linking libm

--- ARM-CMSIS/create_symlinks.sh
+++ ARM-CMSIS/create_symlinks.sh
@@ -7,10 +7,24 @@ CMSIS_ROOT="$1"
 
 [[ $CMSIS_ROOT != "" ]] || exit 1
 
+# Keep this list in sync with CMakeLists.txt
+sources=(
+    DSP/Source/BasicMathFunctions/arm_add_q15.c
+    DSP/Source/BasicMathFunctions/arm_offset_q15.c
+    DSP/Source/BasicMathFunctions/arm_scale_q15.c
+    DSP/Source/MatrixFunctions/arm_mat_init_q15.c
+    DSP/Source/MatrixFunctions/arm_mat_mult_fast_q15.c
+    DSP/Source/StatisticsFunctions/arm_max_q15.c
+    DSP/Source/StatisticsFunctions/arm_min_q15.c
+    DSP/Source/SupportFunctions/arm_fill_q15.c
+)
+
 mkdir -p Core DSP/Source
 ln -sf "$CMSIS_ROOT"/Core/Include Core/
 ln -sf "$CMSIS_ROOT"/DSP/Include DSP/
 ln -sf "$CMSIS_ROOT"/DSP/PrivateInclude DSP/
-for component in BasicMathFunctions MatrixFunctions StatisticsFunctions SupportFunctions; do
-    ln -sf "$CMSIS_ROOT"/DSP/Source/$component DSP/Source/
+for f in ${sources[@]}; do
+    dir="$(dirname $f)"
+    mkdir -p "$dir"
+    ln -sf "$CMSIS_ROOT/$f" "$dir"
 done
