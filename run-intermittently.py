3846d287e49b85e2132f4d3876f66761957b9e75
Chih-Hsuan Yen <yan12125@gmail.com>
2020-09-24 12:20:47 +0800

    run-intermittently.py: run forever until a bug is found

--- run-intermittently.py
+++ run-intermittently.py
@@ -1,19 +1,30 @@
+import os.path
 from subprocess import Popen, TimeoutExpired
 import signal
 import sys
+import tempfile
 
-def main():
+def run_one_inference(logfile):
     while True:
-        with Popen([sys.argv[1], '1']) as proc:
+        with Popen([sys.argv[1], '1'], stdout=logfile, stderr=logfile) as proc:
             try:
-                proc.wait(timeout=0.001)
+                outs, errs = proc.communicate(timeout=0.01)
             except TimeoutExpired:
                 proc.send_signal(signal.SIGINT)
             proc.wait()
             if proc.returncode in (1, -signal.SIGFPE):
                 raise Exception('Crashed')
             if proc.returncode == 0:
-                break
+                logfile.seek(-2000, 2)
+                last_1000 = logfile.read()
+                print('\n'.join(last_1000.decode('ascii').split('\n')[-20:]))
+                return
+
+def main():
+    logfile_path = os.path.join(tempfile.gettempdir(), 'intermittent-cnn')
+    while True:
+        with open(logfile_path, mode='w+b') as logfile:
+            run_one_inference(logfile)
 
 
 if __name__ == '__main__':
