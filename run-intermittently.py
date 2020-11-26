85a8f4bcfab4eaaf197d11cf3104f64e9da8b29b
Chih-Hsuan Yen <yan12125@gmail.com>
2020-11-26 20:15:43 +0800

    run-intermittently: add arguments --interval and --rounds
    And accept non-debug builds (outputs < 2000 bytes)

--- run-intermittently.py
+++ run-intermittently.py
@@ -1,30 +1,42 @@
+import argparse
 import os.path
-from subprocess import Popen, TimeoutExpired
 import signal
-import sys
 import tempfile
+from subprocess import Popen, TimeoutExpired
 
-def run_one_inference(logfile):
+def run_one_inference(program, interval, logfile):
     while True:
-        with Popen([sys.argv[1], '1'], stdout=logfile, stderr=logfile) as proc:
+        with Popen([program, '1'], stdout=logfile, stderr=logfile) as proc:
             try:
-                outs, errs = proc.communicate(timeout=0.01)
+                outs, errs = proc.communicate(timeout=interval)
             except TimeoutExpired:
                 proc.send_signal(signal.SIGINT)
             proc.wait()
             if proc.returncode in (1, -signal.SIGFPE):
                 raise Exception('Crashed')
             if proc.returncode == 0:
-                logfile.seek(-2000, 2)
+                logfile.seek(0, 2)
+                file_size = logfile.tell()
+                logfile.seek(-(2000 if file_size > 2000 else file_size), 2)
                 last_1000 = logfile.read()
                 print('\n'.join(last_1000.decode('ascii').split('\n')[-20:]))
                 return
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--rounds', type=int, default=0)
+    parser.add_argument('--interval', type=float, default=0.01)
+    parser.add_argument('program')
+    args = parser.parse_args()
+
     logfile_path = os.path.join(tempfile.gettempdir(), 'intermittent-cnn')
+    rounds = 0
     while True:
         with open(logfile_path, mode='w+b') as logfile:
-            run_one_inference(logfile)
+            run_one_inference(args.program, args.interval, logfile)
+            rounds += 1
+            if args.rounds and rounds >= args.rounds:
+                break
 
 
 if __name__ == '__main__':
