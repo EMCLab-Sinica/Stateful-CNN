51d8771e00b1b5ba5ec139bfc2aaf3b631e3f6e9
Chih-Hsuan Yen <yan12125@gmail.com>
2020-12-24 21:35:25 +0800

    KWS: Add value_info

--- data/convert-kws.sh
+++ data/convert-kws.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+# Needs patched tf2onnx https://github.com/yan12125/tensorflow-onnx/tree/add_value_info
 python -m tf2onnx.convert --input data/ML-KWS-for-MCU/Pretrained_models/DNN/DNN_S.pb --inputs wav_data:0 --outputs labels_softmax:0 --output data/KWS-DNN_S.onnx
