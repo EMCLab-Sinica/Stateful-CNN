377eb985f9dd407ce1222241c3a27b1dcc2caa34
Chih-Hsuan Yen <yan12125@gmail.com>
2021-03-02 14:10:50 +0800

    Make data from 4mW more stable and update plots

--- data/generate.py
+++ data/generate.py
@@ -1,5 +1,6 @@
+import enum
 import pathlib
-import textwrap
+import subprocess
 import sys
 
 import numpy as np
@@ -9,7 +10,7 @@ import pandas as pd
 
 matplotlib.rcParams.update({
     'errorbar.capsize': 5,
-    'font.size': 14,
+    'font.size': 12,
 })
 
 MOTIVATION_CONFIG = 'mnist'
@@ -22,6 +23,10 @@ def calculate_avg_stdev(df):
     df['Average'] = average
     df['Stdev'] = stdev
     return df
+
+class XAxisType(enum.Enum):
+    GRANULARITY = enum.auto()
+    POWER = enum.auto()
 
 def plot(df, device, variant, outdir):
     df = df[df['device'] == device]
@@ -37,25 +42,21 @@ def plot(df, device, variant, outdir):
 
     n_configs = df['config'].nunique()
     n_methods = df.query('method != "Baseline"')['method'].nunique()
-    fig, axs = plt.subplots(n_configs)
+    n_batches = df['batch'].nunique()
+    fig, axs = plt.subplots(1, n_configs)
     if n_configs == 1:
-        plot_height = 3
+        # width, height
+        fig.set_size_inches(5, 3)
     elif n_configs == 3:
-        plot_height = 10
+        fig.set_size_inches(11, 4)
     else:
         raise ValueError('Unexpected number of configurations!')
-    fig.set_size_inches(6, plot_height)
     plots = []
 
     width = 1.0 / (1 + n_methods)     # the width of the bars
     ind = np.arange(N) + 2 * width    # the x locations for the groups
-    hatches = ['', 'o', r'\\', '..']
-    xs = np.linspace(-0.25, 3.25, 200)
-
-    titles = {
-        'stable': 'Stable power',
-        'unstable': 'Intermittent execution (100uF)',
-    }
+    hatches = ['', '--', r'\\', 'xx']
+    xs = np.linspace(-0.25, N + 0.25, 200)
 
     def job_desc(job):
         if job == 1:
@@ -64,22 +65,29 @@ def plot(df, device, variant, outdir):
 
     methods = ['HAWAII', 'JAPARI', 'Stateful']
 
-    x_axis_use_power = variant == 'unstable' and n_configs > 1
+    if variant == 'unstable' and n_configs > 1:
+        x_axis = XAxisType.POWER
+    else:
+        x_axis = XAxisType.GRANULARITY
 
-    for idx, config in enumerate(config_names.keys()):
+    for idx_config, config in enumerate(config_names.keys()):
         if n_configs == 1:
             ax = axs
         else:
-            ax = axs[idx]
+            ax = axs[idx_config]
         current = df[df['config'] == config]
         if current.empty:
             continue
 
-        ax.set_ylim([0, current['Average'].max() * 1.3])
-
-        if x_axis_use_power:
-            current = current.sort_values('power')
+        if n_batches == 2:
+            p = current.query('method == "Baseline"')['Average'].iat[0]
+            ax.set_ylim([0, p * 4.5])
         else:
+            ax.set_ylim([0, current['Average'].max() * 1.3])
+
+        if x_axis == XAxisType.POWER:
+            current = current.sort_values('power')
+        elif x_axis == XAxisType.GRANULARITY:
             current = current.sort_values('batch')
         baseline_data = current.query('method == "Baseline"')
         has_baseline = variant == 'stable'
@@ -89,38 +97,39 @@ def plot(df, device, variant, outdir):
             ax.bar(x=0, height=0, width=width, bottom=0, fill=False, hatch=hatches[0])
         stateful_time = current.query('method == "Stateful"')['Average']
         max_height = current.max()['Average']
-        for idx, method in enumerate(methods):
+        for idx_method, method in enumerate(methods):
             data = current[current['method'] == method]
             if data.empty:
                 continue
             kwargs = {
-                'x': ind + width * idx,
+                'x': ind + width * idx_method,
                 'height': data['Average'],
                 'width': width,
                 'fill': False,
-                'hatch': hatches[idx + 1],
+                'hatch': hatches[idx_method + 1],
             }
             if not has_baseline:
                 kwargs['yerr'] = data['Stdev']
             plots.append(ax.bar(**kwargs))
 
             # Based on a function from Daniel Tsai
-            if x_axis_use_power:
+            if x_axis == XAxisType.POWER:
                 for value_idx, height in enumerate(data['Average']):
                     if not data.query('method == "Stateful"').empty or not height:
                         continue
-                    yerr = list(data['Stdev'])[value_idx]
-                    xy = (value_idx + 0.4 + width * idx, height + max_height * 0.04 + yerr)
-                    ax.annotate('-{}%'.format(int((1 - list(stateful_time)[value_idx] / height) * 100)),
-                                xy=xy, xycoords='data', annotation_clip=False)
+                    yerr = data['Stdev'].iat[value_idx]
+                    xy = (value_idx + 0.38 + width * idx_method, height + max_height * 0.03 + yerr)
+                    ax.annotate('-{}%'.format(int((1 - stateful_time.iat[value_idx] / height) * 100)),
+                                xy=xy, xycoords='data', annotation_clip=False, fontsize='small')
 
         if n_configs > 1:
             ax.set_title(config_names[config])
         # Put ticks at the middle bar - average of first bar (2) and the last bar (n_methods + 1)
-        ax.set_xticks(np.concatenate(([0], np.arange(N) + (2 + (n_methods + 1)) / 2 * width), axis=None))
-        if x_axis_use_power:
+        x_ticks = np.concatenate(([0], np.arange(N) + (2 + (n_methods + 1)) / 2 * width), axis=None)
+        ax.set_xticks(x_ticks)
+        if x_axis == XAxisType.POWER:
             x_labels = [f'{power}mW' for power in current.query('method == "HAWAII"')['power']]
-        else:
+        elif x_axis == XAxisType.GRANULARITY:
             x_labels = [job_desc(job) for job in current.query('method == "HAWAII"')['batch']]
         if has_baseline or n_configs == 1:
             x_labels = ['Ideal'] + x_labels
@@ -132,26 +141,25 @@ def plot(df, device, variant, outdir):
             horiz_line_data = np.array([baseline_data['Average'] for i in range(len(xs))])
             ax.plot(xs, horiz_line_data, linestyle='--', color='black')
 
-        ax.set(ylabel='Inference time (seconds)')
+        if idx_config == 0:
+            ax.set(ylabel='Inference time (seconds)')
 
-        ax.legend(plots, methods, fontsize='small')
+        if n_configs == 1:
+            ax.legend(plots, methods)
         ax.autoscale_view()
 
-    if not x_axis_use_power:
-        plt.xlabel('Progress preservation granularity')
+    if n_configs > 1:
+        plt.figlegend(plots, methods, ncol=len(methods))
 
-    title = titles.get(variant)
-    if title:
-        plt.suptitle('\n'.join(textwrap.wrap(title, 50)), y=0.98)
-
-    plt.subplots_adjust(left=0.2, bottom=0.2, right=0.95, top=0.9, hspace=0.5)
+    plt.subplots_adjust(left=0.15, bottom=0.1, right=1, top=0.8, wspace=0.2)
 
     filename = f'{device}-{variant}'
-    if n_configs == 1:
+    if n_configs == 1 and n_batches != 2:
         filename += '-motivation'
     filename = 'chart-' + filename
     plt.savefig(outdir / (filename + '.png'))
     plt.savefig(outdir / (filename + '.pdf'))
+    subprocess.check_call(['pdfcrop', outdir / (filename + '.pdf'), outdir / (filename + '-cropped.pdf')])
 
 def main():
     df = pd.read_csv(pathlib.Path(__file__).parent / 'data.csv')
@@ -165,7 +173,7 @@ def main():
         unstable_data = calculate_avg_stdev(df.query('power > 0 & batch == 1'))
         plot(unstable_data, device, 'unstable', outdir)
 
-        stable_data = calculate_avg_stdev(df.query('power == 0'))
+        stable_data = calculate_avg_stdev(df.query('(power == 0 & batch == 1) | method == "Baseline"'))
         plot(stable_data, device, 'stable', outdir)
 
 if __name__ == '__main__':
