b6df6b0f2b5d380194f234be10abae2a43e98b28
Chih-Hsuan Yen <yan12125@gmail.com>
2021-04-09 19:04:44 +0800

    parse_model: dump more stuffs
    To help understand shape inference and debugging
    
    Additions:
    
    * model opset version
    * value info for intermediate results
    * shape for inputs
    * node attribute values

--- utils/parse_model/parse_model.c
+++ utils/parse_model/parse_model.c
@@ -1,11 +1,62 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <inttypes.h>
 #include "external/onnx.proto3.pb-c.h"
 #include "utils.h"
 
+static void handle_value_info(Onnx__ValueInfoProto *value_info) {
+    size_t i;
+
+    Onnx__TensorShapeProto *shape = value_info->type->tensor_type->shape;
+    if (!shape || !shape->n_dim) {
+        return;
+    }
+    printf("name = %s, shape = (", value_info->name);
+    for (i = 0; i < shape->n_dim; i++) {
+        Onnx__TensorShapeProto__Dimension *d = shape->dim[i];
+        switch (d->value_case) {
+            case ONNX__TENSOR_SHAPE_PROTO__DIMENSION__VALUE_DIM_VALUE:
+                printf("%" PRId64, d->dim_value);
+                break;
+            case ONNX__TENSOR_SHAPE_PROTO__DIMENSION__VALUE_DIM_PARAM:
+                printf("%s", d->dim_param);
+                break;
+            default:
+                break;
+        }
+        if (i != shape->n_dim - 1) {
+            printf(", ");
+        }
+    }
+    printf(")\n");
+}
+
+static void handle_tensor(Onnx__TensorProto *tensor) {
+    size_t i;
+
+    printf("name=%s, dims=[", tensor->name);
+    for (i = 0; i < tensor->n_dims; i++) {
+        printf("%" PRId64, tensor->dims[i]);
+        if (i != tensor->n_dims - 1) {
+            printf(", ");
+        }
+    }
+    printf("], ");
+
+    switch (tensor->data_type) {
+        case ONNX__TENSOR_PROTO__DATA_TYPE__INT64:
+            printf("type=INT64");
+            break;
+        default:
+            printf("(Tensor data type %d not implemented)", tensor->data_type);
+            break;
+    }
+}
+
 int main(void) {
   Onnx__ModelProto *msg;
-  size_t i, j;
+  size_t i, j, k;
 
   uint8_t *buf = malloc(MAX_MSG_SIZE);
   size_t msg_len = read_buffer(MAX_MSG_SIZE, buf);
@@ -21,10 +72,21 @@ int main(void) {
     return 1;
   }
 
+  for (i = 0; i < msg->n_opset_import; i++) {
+      Onnx__OperatorSetIdProto *opset_import = msg->opset_import[i];
+      if (!strlen(opset_import->domain)) {
+          printf("%s", "ai.onnx");
+      } else {
+          printf("%s", opset_import->domain);
+      }
+      printf(" v%" PRId64 "\n", opset_import->version);
+      break;
+  }
+
   Onnx__GraphProto *graph = msg->graph;
-  printf("Inputs:\n");
+  printf("\nInputs:\n");
   for (i = 0; i < graph->n_input; i++) {
-      printf("%s\n", graph->input[i]->name);
+      handle_value_info(graph->input[i]);
   }
   printf("\nNodes:\n");
   for (i = 0; i < graph->n_node; i++) {
@@ -38,18 +100,49 @@ int main(void) {
       for (j = 0; j < n->n_output; j++) {
           printf("\t\t%s\n", n->output[j]);
       }
+      printf("\tNode attributes:\n");
+      for (j = 0; j < n->n_attribute; j++) {
+          Onnx__AttributeProto *attr = n->attribute[j];
+              printf("\t\t%s = ", attr->name);
+          switch (attr->type) {
+              case ONNX__ATTRIBUTE_PROTO__ATTRIBUTE_TYPE__FLOAT:
+                  printf("FLOAT:%f", attr->f);
+                  break;
+              case ONNX__ATTRIBUTE_PROTO__ATTRIBUTE_TYPE__INT:
+                  printf("INT:%" PRId64, attr->i);
+                  break;
+              case ONNX__ATTRIBUTE_PROTO__ATTRIBUTE_TYPE__TENSOR:
+                  printf("TENSOR:");
+                  handle_tensor(attr->t);
+                  break;
+              case ONNX__ATTRIBUTE_PROTO__ATTRIBUTE_TYPE__STRING:
+                  printf("STRING:%s", attr->s.data);
+                  break;
+              case ONNX__ATTRIBUTE_PROTO__ATTRIBUTE_TYPE__INTS:
+                  printf("INTS:[");
+                  for (k = 0; k < attr->n_ints; k++) {
+                      printf("%" PRId64, attr->ints[k]);
+                      if (k != attr->n_ints - 1) {
+                          printf(", ");
+                      }
+                  }
+                  printf("]");
+                  break;
+              default:
+                  printf("(Attribute type %d not implemented)", attr->type);
+                  break;
+          }
+          printf("\n");
+      }
   }
   printf("\nInitializers:\n");
   for (i = 0; i < graph->n_initializer; i++) {
-      Onnx__TensorProto *t = graph->initializer[i];
-      printf("name = %s, dims = [", t->name);
-      for (j = 0; j < t->n_dims; j++) {
-          printf("%d", t->dims[j]);
-          if (j != t->n_dims - 1) {
-              printf(", ");
-          }
-      }
-      printf("]\n");
+      handle_tensor(graph->initializer[i]);
+      printf("\n");
+  }
+  printf("\nValue info:\n");
+  for (i = 0; i < graph->n_value_info; i++) {
+      handle_value_info(graph->value_info[i]);
   }
 
   onnx__model_proto__free_unpacked(msg, NULL);
