ab22c96c208a38fe0bd18df362460bf73326c1cb
Chih-Hsuan Yen <yan12125@gmail.com>
2021-07-01 17:00:49 +0800

    Move from keras-onnx to tf2onnx
    The former is incompatible with Tensorflow 2.4+ [1]
    
    [1] https://github.com/onnx/keras-onnx/issues/651

--- utils/export_squeezenet_cifar10.py
+++ utils/export_squeezenet_cifar10.py
@@ -3,13 +3,13 @@ import sys
 
 import tensorflow as tf
 import onnx
-import keras2onnx
+import tf2onnx.convert
 
 with open(os.path.join(sys.argv[1], 'models', 'squeeze_net.json')) as f:
     model_json = f.read()
 model = tf.keras.models.model_from_json(model_json)
 model.load_weights(os.path.join(sys.argv[1], 'models', 'squeeze_net.h5'))
 
-onnx_model = keras2onnx.convert_keras(model, model.name)
+onnx_model, _ = tf2onnx.convert.from_keras(model)
 
 onnx.save(onnx_model, 'squeezenet_cifar10.onnx')
